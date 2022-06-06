257e56966e0b88cae2e6fd165cbe9c7badcc7aae
whohyno <90461313+whohyno@users.noreply.github.com>
2022-06-06 11:43:08 +0300

    Update SPTS5.lua

--- SPTS5.lua
+++ SPTS5.lua
@@ -227,6 +227,8 @@ local cf = Instance.new("Frame")
 local c1 = Instance.new("TextLabel")
 local c = Instance.new("TextButton")
 local t = 10
+local hue = tick() % t / t
+local color = Color3.fromHSV(hue, 1, 1)
 local DeathReturn = Instance.new("TextButton")
 local PunchMode = Instance.new("TextButton")
 local WayPoints = Instance.new("TextButton")
@@ -1697,12 +1699,8 @@ end)
 DeathReturn.MouseButton1Click:Connect(function()
 	if deathreturnactive ~= true then
 		deathreturnactive = true
+		DeathReturn.BackgroundColor3 = color
 		DeathReturn.Text = "OnDeath Return: ON"
-		wait(10)
-		while wait() do
-			local hue = tick() % t / t
-			local color = Color3.fromHSV(hue, 1, 1)
-			DeathReturn.BackgroundColor3 = color
 	else
 		deathreturnactive = false
 		DeathReturn.BackgroundColor3 = Color3.new(0.1, 0.1, 0.1)
