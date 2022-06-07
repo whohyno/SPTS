643cfd085111c725dc44bdebf618aaf13911a867
whohyno <90461313+whohyno@users.noreply.github.com>
2022-06-07 08:45:33 +0300

    Create tepup.lua

--- tepup.lua
+++ tepup.lua
@@ -1710,16 +1710,16 @@ DeathReturn.MouseButton1Click:Connect(function()
 	if deathreturnactive ~= true then
 		deathreturnactive = true
 		DeathReturn.Text = "OnDeath Return: ON"
-			
+
 spawn(function()
 	while task.wait() do
-		if deathreturnactive ~= true then
 		local t = 5; 
 		local hue = tick() % t / t
 		local colorrr = Color3.fromHSV(hue, 1, 1)
 		DeathReturn.BackgroundColor3 = colorrr
 	end
 end)
+				
 	else
 		deathreturnactive = false
 		DeathReturn.BackgroundColor3 = Color3.new(0.1, 0.1, 0.1)
