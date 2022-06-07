284249332ede3f2ab2609ec162b2753cc6dee33f
whohyno <90461313+whohyno@users.noreply.github.com>
2022-06-07 22:21:14 +0300

    Update SPTS.lua

--- SPTS.lua
+++ SPTS.lua
@@ -443,7 +443,7 @@ Logo.BackgroundColor3 = Color3.new(1, 1, 1)
 Logo.BackgroundTransparency = 1
 Logo.Position = UDim2.new(0, 5, 0, 5)
 Logo.Size = UDim2.new(0, 75, 0, 75)
-Logo.Image = "rbxassetid://9846436903"
+Logo.Image = "rbxassetid://9846436847"
 Logo.ZIndex = 1
 
 spawn(function()
