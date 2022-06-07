77be6f46efa83c628dd9ec25c251578fcedba3c3
whohyno <90461313+whohyno@users.noreply.github.com>
2022-06-07 08:18:49 +0300

    Update tepup.lua

--- tepup.lua
+++ tepup.lua
@@ -116,7 +116,7 @@ function CreateESP(plr)
 		TxtHealth.ZIndex = 10
 		TxtHealth.Text = ""
 		TxtHealth.BackgroundTransparency = 1
-		TxtHealth.Position = UDim2.new(0, 0, 0, -25)
+		TxtHealth.Position = UDim2.new(0, 0, 0, -35)
 		TxtHealth.Size = UDim2.new(1, 0, 10, 0)
 		TxtHealth.Font = "SourceSansBold"
 		TxtHealth.TextColor3 = Color3.new(0, 0, 0)
@@ -128,7 +128,7 @@ function CreateESP(plr)
 		TxtFist.ZIndex = 10
 		TxtFist.Text = ""
 		TxtFist.BackgroundTransparency = 1
-		TxtFist.Position = UDim2.new(0, 0, 0, -15)
+		TxtFist.Position = UDim2.new(0, 0, 0, -25)
 		TxtFist.Size = UDim2.new(1, 0, 10, 0)
 		TxtFist.Font = "SourceSansBold"
 		TxtFist.TextColor3 = Color3.new(0, 0, 0)
@@ -140,7 +140,7 @@ function CreateESP(plr)
 		TxtBody.ZIndex = 10
 		TxtBody.Text = ""
 		TxtBody.BackgroundTransparency = 1
-		TxtBody.Position = UDim2.new(0, 0, 0, -5)
+		TxtBody.Position = UDim2.new(0, 0, 0, -15)
 		TxtBody.Size = UDim2.new(1, 0, 10, 0)
 		TxtBody.Font = "SourceSansBold"
 		TxtBody.TextColor3 = Color3.new(0, 0, 0)
@@ -1709,11 +1709,26 @@ DeathReturn.MouseButton1Click:Connect(function()
 	if deathreturnactive ~= true then
 		deathreturnactive = true
 		DeathReturn.Text = "OnDeath Return: ON"
-		DeathReturn.BackgroundColor3 = Color3.new(0, 0.5, 0)
+			
+spawn(function()
+	while task.wait() do
+		if deathreturnactive ~= true then
+		local t = 5; 
+		local hue = tick() % t / t
+		local color = Color3.fromHSV(hue, 1, 1)
+		DeathReturn.BackgroundColor3 = color
+	end
+end)
 	else
 		deathreturnactive = false
 		DeathReturn.Text = "OnDeath Return: OFF"
+				
+spawn(function()
+	while task.wait() do
+		if deathreturnactive ~= false then
 		DeathReturn.BackgroundColor3 = Color3.new(0.1, 0.1, 0.1)
+	end
+end)
 	end
 end)
 					
