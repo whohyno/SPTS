2beb061f34f45e8c8304ef6b5fce129040dfd362
whohyno <90461313+whohyno@users.noreply.github.com>
2022-06-07 22:19:43 +0300

    Update SPTS.lua

--- SPTS.lua
+++ SPTS.lua
@@ -443,8 +443,14 @@ Logo.BackgroundColor3 = Color3.new(1, 1, 1)
 Logo.BackgroundTransparency = 1
 Logo.Position = UDim2.new(0, 5, 0, 5)
 Logo.Size = UDim2.new(0, 75, 0, 75)
-Logo.Image = "rbxassetid://1560823444"
+Logo.Image = "rbxassetid://9846436903"
 Logo.ZIndex = 1
+
+spawn(function()
+	while task.wait() do
+		Logo.ImageColor3 = colorrr
+	end
+end)
 
 ShowLocation.Name = "ShowLocation"
 ShowLocation.Parent = WayPointsFrame
