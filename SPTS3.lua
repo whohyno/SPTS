d1c240a56807cadca1e4273b38bf7c713c7e7e11
whohyno <90461313+whohyno@users.noreply.github.com>
2022-06-16 17:43:59 +0300

    Create SPTS3.lua

---
+++ SPTS3.lua
@@ -0,0 +1,5 @@
+ok you're wondering why this script doesn't work, here's why:
+this is actually a link to another github that has a lot of SPTS scripts and it's pure fuckin' gold.
+everything there is good, here's a link:
+
+https://github.com/theplayer8000/gui-spts.scripts/
